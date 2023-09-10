6f241184b1d7258ca357b7cf69a931b43ceabad2
Fortune Minaseidiema <fminaseidiema26@gmail.com>
2023-09-10 21:15:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+class Profile(BaseModel):
+    slack_name: str
+    current_day: str
+    utc_time: str
+    track: str
+    github_file_url: str
+    github_repo_url: str
+    status_code: int
+
+
+@app.get("/")
+async def profile(slack_name: str = "Fortune_Mina", track: str = "backend"):
+    
+    current_utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    day = datetime.now().strftime("%A")
+    
+    user_profile = Profile(
+        slack_name = slack_name,
+        current_day = day,
+        utc_time = current_utc_time,
+        track = track,
+        github_file_url = "str",
+        github_repo_url = "str",
+        status_code = 200,
+    )
+    
+    return user_profile.dict()
+    

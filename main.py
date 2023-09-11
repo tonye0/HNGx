c4d08a3debabb9038db9c005ea0a3ddfd4577e42
Fortune Minaseidiema <fminaseidiema26@gmail.com>
2023-09-11 08:15:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,14 +19,16 @@ async def profile(slack_name: str = "Fortune_Mina", track: str = "backend"):
     
     current_utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
     day = datetime.now().strftime("%A")
+    file_url = "https://github.com/tonye0/HNGx/blob/master/main.py"
+    repo_url = "https://github.com/tonye0/HNGx/tree/master.git"
     
     user_profile = Profile(
         slack_name = slack_name,
         current_day = day,
         utc_time = current_utc_time,
         track = track,
-        github_file_url = "str",
-        github_repo_url = "str",
+        github_file_url = file_url,
+        github_repo_url = repo_url,
         status_code = 200,
     )
     
